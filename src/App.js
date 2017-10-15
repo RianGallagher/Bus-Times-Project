f6c63abc3f8139da54e74dc9cacf1266fcf7e835
u180488 <rian.gallagher.2016@mumail.ie>
2017-10-15 14:11:17 +0100

    Experimenting with state change in react and firebase db

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import * as firebase from 'firebase';
 
 class App extends Component {
+
+    constructor(){
+        super();
+        this.state = {
+            speed: 10
+        };
+    }
+
+    componentDidMount(){
+        const rootRef = firebase.database().ref().child('Time');
+        const timeRef = rootRef.child('66');
+
+        timeRef.on('value', snap => {
+            this.setState({
+                speed: snap.val()
+            });
+        });
+    }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>{this.state.speed}</h1>
       </div>
     );
   }
