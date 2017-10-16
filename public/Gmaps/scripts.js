1e7478eeeb921d3c2301ba88328ec0f391d8f326
shamjam96 <seosamhmc96@gmail.com>
2017-10-17 00:05:21 +0100

    Started Google Maps

---
+++ public/Gmaps/scripts.js
@@ -0,0 +1,44 @@
+
+      var map;
+      var infowindow;
+
+	  var lat = 53.376158;
+	  var lng= -6.586436;
+      function initMap() {
+        var myLocation = {lat: lat, lng:lng};
+
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: myLocation,
+          zoom: 15
+        });
+
+        infowindow = new google.maps.InfoWindow();
+        var service = new google.maps.places.PlacesService(map);
+        var request= {
+			location: myLocation,
+			radius: '500',
+			query: 'bus stop'
+		};
+			service.textSearch(request, callback);
+      }
+
+      function callback(results, status) {
+        if (status === google.maps.places.PlacesServiceStatus.OK) {
+          for (var i = 0; i < results.length; i++) {
+            createMarker(results[i]);
+          }
+        }
+      }
+
+      function createMarker(place) {
+        var placeLoc = place.geometry.location;
+        var marker = new google.maps.Marker({
+          map: map,
+          position: place.geometry.location
+        });
+
+        google.maps.event.addListener(marker, 'click', function() {
+          infowindow.setContent(place.name);
+          infowindow.open(map, this);
+        });
+      }
