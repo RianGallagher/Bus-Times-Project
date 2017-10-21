6a3a5797f6bb38f195290b2814b996600995cc53
Glenn Purtill <glennpurtill@Glenns-MacBook-Pro.local>
2017-10-21 18:54:31 +0100

    Merged files

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
-import ReactTable from 'react-table'
-import 'react-table/react-table.css'
+import './index.js';
+import * as firebase from 'firebase';
+
+
 
 class App extends React.Component {
   constructor(props) {
@@ -10,12 +12,26 @@ class App extends React.Component {
 
     this.state = {
       results: [],
-      busStopNumber: ''
+      busStopNumber: '',
+      route: '',
+      destination: '',
+      oirigin: '',
+      duetime: '',
     };
 
     // For entering bus stop numbers: https://reactjs.org/docs/forms.html
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    const config = {
+      apiKey: "AIzaSyCEjs14JwhDwrnao-S_dmCqFNTEgVFQ9Qo",
+      authDomain: "dublinbusproject.firebaseapp.com",
+      databaseURL: "https://dublinbusproject.firebaseio.com",
+      projectId: "dublinbusproject",
+      storageBucket: "dublinbusproject.appspot.com",
+      messagingSenderId: "655945553210"
+  };
+  
+  firebase.initializeApp(config);
   }
 
   handleChange(event) {
@@ -28,29 +44,21 @@ class App extends React.Component {
       const results = res.data.results;
       this.setState({ results });
     });
+    
+    const rootRef = firebase.database().ref();
+    for(var i =0; i<this.state.results.length; i++){
+      rootRef.child(this.state.busStopNumber).child(i).child("duetime").set(this.state.results[i].duetime);
+      rootRef.child(this.state.busStopNumber).child(i).child("route").set(this.state.results[i].route);
+      rootRef.child(this.state.busStopNumber).child(i).child("destination").set(this.state.results[i].destination);
+      rootRef.child(this.state.busStopNumber).child(i).child("origin").set(this.state.results[i].origin);
+    }
     event.preventDefault();
+  
+  
   }
-
-  componentDidMount() {
-  }
+  
 
   render() {
-    const columns = [{
-      Header: 'Route',
-      accessor: 'route' // String-based value accessors!
-    },
-    {
-      Header: 'Origin',
-      accessor: 'origin' // String-based value accessors!
-    },
-    {
-      Header: 'Destination',
-      accessor: 'destination' // String-based value accessors!
-    },
-    {
-      Header: 'Due In (Minutes)',
-      accessor: 'duetime' // String-based value accessors!
-    }]
     return (
       <div>
         <form onSubmit={this.handleSubmit}>
@@ -59,11 +67,9 @@ class App extends React.Component {
           <input type="text" value={this.state.busStopNumber} onChange={this.handleChange} />
           </label>
           <input type="submit" value="Go" />
+          <h1>{this.state.speed}</h1>
         </form>
-        <ReactTable
-          data={this.state.results}
-          columns={columns}
-        />
+       
       </div>
     );
   }
