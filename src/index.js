03e83392ab15110b8f73a1ee573e05b905cab7e5
u180488 <rian.gallagher.2016@mumail.ie>
2017-10-24 13:40:24 +0100

    store bus times in DB

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import axios from 'axios';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import * as firebase from 'firebase';
+
+const config = {
+    apiKey: "AIzaSyCEjs14JwhDwrnao-S_dmCqFNTEgVFQ9Qo",
+    authDomain: "dublinbusproject.firebaseapp.com",
+    databaseURL: "https://dublinbusproject.firebaseio.com",
+    projectId: "dublinbusproject",
+    storageBucket: "dublinbusproject.appspot.com",
+    messagingSenderId: "655945553210"
+};
+
+firebase.initializeApp(config);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
