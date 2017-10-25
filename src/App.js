a26ebf0695aee6d4c1e0a4abfc6bf6d2aa2acb31
u180488 <rian.gallagher.2016@mumail.ie>
2017-10-25 12:00:22 +0100

    Put dublin bus times in DB and then display from DB

--- src/App.js
+++ src/App.js
@@ -18,50 +18,51 @@ class App extends React.Component {
       duetime: '',
       speed: ''
     };
-    
+
     this.handleChange = this.handleChange.bind(this);
-    
    this.handleSubmit = this.handleSubmit.bind(this);
+   this.startTimer = this.startTimer.bind(this);
   }
 
-  handleChange(event) {
+  handleChange() {
     //this.setState({ busStopNumber: event.target.value });
     
     this.setState({busStopNumber : document.getElementById('stopNum').value});
     
   }
 
-  
-
-  handleSubmit(event) {
-    axios.get(`https://data.dublinked.ie/cgi-bin/rtpi/realtimebusinformation?stopid=${this.state.busStopNumber}&format=json`)
-    .then(res => {
-      console.log(this.state.busStopNumber);
-      //Make it read from Database instead of setting state
-      const results = res.data.results;
-      //this.setState({ results: results });
-        const stopRef = firebase.database().ref().child(this.state.busStopNumber);
-        console.log(this.state.results);
-        for(var i =0; i<results.length; i++){
-            stopRef.child(i).child("duetime").set(results[i].duetime);
-            stopRef.child(i).child("route").set(results[i].route);
-            stopRef.child(i).child("destination").set(results[i].destination);
-            stopRef.child(i).child("origin").set(results[i].origin);
-        }
-    });
-    
-    this.displayTimes();
-    event.preventDefault();
-  
+  startTimer(event){
+      event.preventDefault();
+      this.handleSubmit();
+      setInterval(this.handleSubmit, 10000);
   }
 
+  handleSubmit() {
+
+    axios.get(`https://data.dublinked.ie/cgi-bin/rtpi/realtimebusinformation?stopid=${this.state.busStopNumber}&format=json`)
+        .then(res => {
+            console.log("refreshing times");
+            //Make it read from Database instead of setting state
+            const results = res.data.results;
+            //this.setState({ results: results });
+            const stopRef = firebase.database().ref().child(this.state.busStopNumber);
+            for (var i = 0; i < results.length; i++) {
+                stopRef.child(i).child("duetime").set(results[i].duetime);
+                stopRef.child(i).child("route").set(results[i].route);
+                stopRef.child(i).child("destination").set(results[i].destination);
+                stopRef.child(i).child("origin").set(results[i].origin);
+            }
+        })
+        .then(this.displayTimes());
+    }
+
   displayTimes(){
-    setTimeout(() => {
+    //setTimeout(() => {
       const stopRef = firebase.database().ref().child(this.state.busStopNumber);
       stopRef.on('value', snapshot =>{
           this.setState({results: snapshot.val()});
-      }); 
-    }, 600);
+      });
+    //}, 600);
   }
   
 
@@ -81,10 +82,10 @@ class App extends React.Component {
           {
               Header: 'Due In (Minutes)',
               accessor: 'duetime' // String-based value accessors!
-          }]
+          }];
     return (
       <div>
-        <form onSubmit={this.handleSubmit}>
+        <form onSubmit={this.startTimer}>
           <label>
             Bus Stop Number:
           <input id="stopNum" type="text" />
