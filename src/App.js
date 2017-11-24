5d1ad1198e3ceba61a16ecca31c506dc32875576
Glenn Purtill <glennpurtill@Glenns-MacBook-Pro.local>
2017-11-24 13:41:41 +0000

    Completed

--- src/App.js
+++ src/App.js
@@ -5,103 +5,91 @@ import * as firebase from 'firebase';
 import ReactTable from 'react-table';
 import 'react-table/react-table.css'
 import Promise from 'promise';
+import './App.css';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      results: [],
-      
+      heading1: '',
+      heading2: ''
     };
 
 
-    this.handleChange = this.handleChange.bind(this);
-  // this.handleSubmit = this.handleSubmit.bind(this);
-    this.displayTimes = this.displayTimes.bind(this);
-    //this.putTimesIntoTable = this.putTimesIntoTable.bind(this);
+    this.doSubmit = this.doSubmit.bind(this);
+    this.displayTimetime = this.displayTimetable.bind(this);
   }
 
-  handleChange(event) {
+  doSubmit(event) {
     var temp = [-1], data = [];
     event.preventDefault();
     console.log("in handle change");
-    //this.setState({busStopNumber : document.getElementById('stopNum').value});
     var num = document.getElementById('stopNum').value;
     console.log(num);
     let myProm = new Promise(function(resolve, reject) {
        const stopRef = firebase.database().ref().child("timetable").child(num);
       stopRef.on('value', snapshot =>{
-          temp = [-1];
+          temp = [null];
           temp = snapshot.val();
           
       });
       setTimeout(function(){
-        if (temp[0]!==-1) {
+        if (temp!==undefined) {
           resolve(temp);
         } else {
           reject('Error');
         }
-       },2000);
+       },1200);
       });
       myProm.then((fromResolve) =>
-        // function(fromResolve) {
-        // data = fromResolve;
-        // console.log(fromResolve);
-       this.displayTimes(fromResolve)
+       this.displayTimetable(fromResolve)
       ).catch(function(fromReject){
         alert(num + " is not a bus route");
       }) 
       
   }
-
-
-  displayTimes(fromResolve){
+  
+  displayTimetable(fromResolve){
      var data = [];
      console.log(fromResolve)
-    var longestArrayLength = Math.max(fromResolve[1].weekdays.length, fromResolve[1].saturday.length, fromResolve[1].sunday.length, fromResolve[2].weekdays.length, fromResolve[2].saturday.length,fromResolve[2].sunday.length);
-    for (var i = 0; i < longestArrayLength; i++) {
-
-    data.push({
-      "weekdayTime1":fromResolve[1].weekdays[i],
-      "saturdayTime1": fromResolve[1].saturday[i],
-      "sundayTime1": fromResolve[1].sunday[i],
-      "weekdayTime2": fromResolve[2].weekdays[i],
-      "saturdayTime2": fromResolve[2].saturday[i],
-      "sundayTime2": fromResolve[2].sunday[i]
-    });
-  }
+     if(fromResolve[2]!==undefined){
+        var longestArrayLength = Math.max(fromResolve[1].weekdays.length, fromResolve[1].saturday.length, fromResolve[1].sunday.length, fromResolve[2].weekdays.length, fromResolve[2].saturday.length,fromResolve[2].sunday.length);
+        for (var i = 0; i < longestArrayLength; i++) {
+        
+        data.push({
+          "weekdayTime1":fromResolve[1].weekdays[i],
+          "saturdayTime1": fromResolve[1].saturday[i],
+          "sundayTime1": fromResolve[1].sunday[i],
+          "weekdayTime2": fromResolve[2].weekdays[i],
+          "saturdayTime2": fromResolve[2].saturday[i],
+          "sundayTime2": fromResolve[2].sunday[i]
+        });
+      }
+      this.setState({heading1: fromResolve[1].heading, heading2: fromResolve[2].heading});
+      console.log(this.state.heading1);
+    }
+    else{
+      var longestArrayLength = Math.max(fromResolve[1].weekdays.length, fromResolve[1].saturday.length, fromResolve[1].sunday.length);
+      for (var i = 0; i < longestArrayLength; i++) {
+      
+      data.push({
+        "weekdayTime1":fromResolve[1].weekdays[i],
+        "saturdayTime1": fromResolve[1].saturday[i],
+        "sundayTime1": fromResolve[1].sunday[i],
+        "weekdayTime2": "-",
+       "saturdayTime2": "-",
+        "sundayTime2": "-"
+      });
+    }
+    this.setState({heading1: fromResolve[1].heading, heading2: "No Service"});
+    console.log(this.state.heading1);
+    }
 
 this.setState({ data });
 
 console.dir(data);
   }
-
-  // putTimesIntoTable() {
-  //   var data = [];
-  //   console.log(this.state.results);
-  //   // console.log(this.state.results[1].saturday);
-  //   // // Replace all mentions of the the weekdayTimes arrays below with however you're accessing data from the database
-  //   // // Afterwards, replace "this.makeCORSRequest()" in the handleSubmit() function with "this.getTimesFromDatabase()"
-
-  //   // var longestArrayLength = Math.max(this.state.results[1].weekdays.length, this.state.results[1].saturday.length, this.state.results[1].sunday.length, this.state.results[2].weekdays.length, this.state.results[2].saturday.length, this.state.results[2].sunday.length);
-  //   // for (var i = 0; i < longestArrayLength; i++) {
-
-  //   //   data.push({
-  //   //     "weekdayTime1": this.state.results[1].weekdays[i],
-  //   //     "saturdayTime1": this.state.results[1].saturday[i],
-  //   //     "sundayTime1": this.state.results[1].sunday[i],
-  //   //     "weekdayTime2": this.state.results[2].weekdays[i],
-  //   //     "saturdayTime2": this.state.results[2].saturday[i],
-  //   //     "sundayTime2": this.state.results[2].sunday[i]
-  //   //   });
-  //   // }
-
-  //   // this.setState({ data });
-
-  //   // console.dir(data);
-  // }
-  
 
   render() {
     const columns1 = [{
@@ -142,12 +130,11 @@ console.dir(data);
         Bus Stop Number:
       <input id="stopNum" type="text" />
       </label>
-      <input type="submit" value="Go" onClick={this.handleChange}/>
+      <input type="submit" value="Go" onClick={this.doSubmit}/>
     </form>
-
         <div id="timetable-container">
           <div class="timetable">
-            <div class="center"><strong>heading</strong></div>
+            <div class="center"><strong>{this.state.heading1}</strong></div>
             <ReactTable
               data={this.state.data}
               columns={columns1}
@@ -155,7 +142,7 @@ console.dir(data);
             />
           </div>
           <div class="timetable">
-            <div class="center"><strong>heading</strong></div>
+            <div class="center"><strong>{this.state.heading2}</strong></div>
             <ReactTable
               data={this.state.data}
               columns={columns2}
